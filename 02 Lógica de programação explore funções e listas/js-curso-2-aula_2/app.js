ab0c9b9c3552cf538327149315167c9165337f19
Wanh3ll <wancleiton.cantanhede@gmail.com>
2025-10-20 22:10:48 -0300

    v1.6

--- 02 Lógica de programação explore funções e listas/js-curso-2-aula_2/app.js
+++ 02 Lógica de programação explore funções e listas/js-curso-2-aula_2/app.js
@@ -1,11 +1,26 @@
+let escolha = prompt("Defina o número máximo para o jogo:");
+let numeroMaximo = 1
+let numeroAleatorio = numeroSecreto();
 function alterarTexto(tag, textoElemento) {
   let elemento = document.querySelector(tag);
   elemento.innerHTML = textoElemento;
 }
 
-alterarTexto("h1", "*** Jogo do número secreto ***");
-alterarTexto("p", "Escolha um número entre 1 e 10");
+console.log(`O numero secreto é ${numeroAleatorio}`);
+alterarTexto("h1", "Jogo do número secreto");
+alterarTexto("p", `Escolha um número entre 1 e ${escolha}`);
 
 function verificarChute() {
-  console.log('O botão foi clicado!');
+  let chute = document.querySelector("input").value;
+  let resultado = (chute == numeroAleatorio);
+  if (resultado != true) {
+    alert("Você errou!");
+  }
+  else {
+    alert("Você acertou!");
+  }  
+}
+
+function numeroSecreto() {
+  return parseInt(Math.random() * numeroMaximo * 10 + 1);
 }
