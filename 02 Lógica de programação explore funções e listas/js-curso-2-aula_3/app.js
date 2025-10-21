f54a047f1bd55cb912438d51b177b3f336c61197
Wanh3ll <wancleiton.cantanhede@gmail.com>
2025-10-20 23:30:44 -0300

    v.2.1

--- 02 Lógica de programação explore funções e listas/js-curso-2-aula_3/app.js
+++ 02 Lógica de programação explore funções e listas/js-curso-2-aula_3/app.js
@@ -1,5 +1,5 @@
 let numeroAleatorio = numeroSecreto();
-
+let tentativas = 0;
 function alterarTexto(tag, textoElemento) {
     let elemento = document.querySelector(tag);
     elemento.innerHTML = textoElemento;
@@ -12,22 +12,24 @@ function verificarChute() {
     let chute = document.querySelector("input").value;
     if (chute == numeroAleatorio) {
         alterarTexto("h1", "Você acertou! 🎉");
-        alterarTexto("p", "Parabéns por acertar o número secreto.");
+        alterarTexto("p", "Parabéns por acertar o número secreto.");       
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        alert(`Isso ai! Você descobriu o número secreto que é ${numeroAleatorio} com ${tentativas} ${palavraTentativa}.`);
     }
     else if (chute < numeroAleatorio) {
         alterarTexto("h1", "Tente Novamente");
         alterarTexto("p", "O número secreto é maior 😮");
+        tentativas++;
     }
     else {
         alterarTexto("h1", "Tente Novamente");
         alterarTexto("p", "O número secreto é menor 😮");
+        tentativas++;
     }
 }
 function numeroSecreto() {
     return parseInt(Math.random() * 10 + 1);
 }
-
-
 
 
 
