47f6d8694562fe438eb5bf914585ec531e6ad219
Wanh3ll <wancleiton.cantanhede@gmail.com>
2025-10-24 21:25:56 -0300

    v2.2

--- 02 Lógica de programação explore funções e listas/js-curso-2-aula_3/app.js
+++ 02 Lógica de programação explore funções e listas/js-curso-2-aula_3/app.js
@@ -5,9 +5,11 @@ function alterarTexto(tag, textoElemento) {
     let elemento = document.querySelector(tag);
     elemento.innerHTML = textoElemento;
 }
-
+let elementoReiniciar = document.getElementById("reiniciar");
+elementoReiniciar.disabled = true; // para que o botão novo jogo inicie desabilitado
 alterarTexto("h1", "Jogo do número secreto");
 alterarTexto("p", "Escolha um número entre 1 e 10");
+limparChute();
 
 function verificarChute() {
     let chute = document.querySelector("input").value;
@@ -15,22 +17,29 @@ function verificarChute() {
         alterarTexto("h1", "Você acertou! 🎉");
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         alterarTexto("p", `Isso ai! Você descobriu o número secreto que é ${numeroAleatorio} com ${tentativas} ${palavraTentativa}.`);       
+        alterarTexto("input", "");
+        elementoReiniciar.disabled = false; // habilita o botão novo jogo ao acertar
         }
     else if (chute < numeroAleatorio) {
         alterarTexto("h1", "Tente Novamente");
         alterarTexto("p", "O número secreto é maior 😮");
+        limparChute();
         tentativas++;
     }
     else {
         alterarTexto("h1", "Tente Novamente");
         alterarTexto("p", "O número secreto é menor 😮");
+        limparChute();
         tentativas++;
     }
 }
 function numeroSecreto() {
     return parseInt(Math.random() * 10 + 1);
 }
-
+function limparChute() {
+    chute = document.querySelector("input");
+    chute.value = "";
+}
 
 
 
